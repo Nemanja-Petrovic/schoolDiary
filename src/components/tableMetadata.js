270691c44995c35082b10a94069b3d6450be1b62
Nemanja Petrovic <kole84ns@gmail.com>
2021-03-17 13:33:44 +0100

    Fix tableMetadata

--- src/components/tableMetadata.js
+++ src/components/tableMetadata.js
@@ -1,10 +1,10 @@
 export const tableInfo = {
   students: [
     { label: "#", key: "_id" },
-    { label: "Average", key: "average" },
     { label: "First Name", key: "firstName" },
     { label: "Last Name", key: "lastName" },
     { label: "Class", key: "className" },
+    { label: "Average", key: "average" },
   ],
   teachers: [
     { label: "First Name", key: "firstName" },
